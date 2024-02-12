9e3ff5140f800757b5dc28a6ceda1a716126c04e
Xerlok <20791727+Xerlok@users.noreply.github.com>
2024-02-12 23:24:43 +0300

    Create basic weatherAPI request

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# template
-A basic template with webpack
+
--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './styles.css';
+
+async function getWeather() {
+  const response = await fetch(
+    'https://api.weatherapi.com/v1/forecast.json?key=29061576e8ad448eaef193306241202&q=Paris&days=3',
+    { mode: 'cors' },
+  );
+  const weatherData = await response.json();
+  console.log(weatherData);
+}
+
+getWeather().catch((err) => { console.log(err); });
