bf1a1633dc51c625a708d99119782b60859254bd
Xerlok <20791727+Xerlok@users.noreply.github.com>
2024-02-14 00:17:06 +0300

    Make basic app layout

--- src/getWeather.js
+++ src/getWeather.js
@@ -1,8 +1,17 @@
 export default async function getWeather() {
-  const response = await fetch(
-    'https://api.weatherapi.com/v1/forecast.json?key=29061576e8ad448eaef193306241202&q=Paris&days=3',
-    { mode: 'cors' },
-  );
-  const weatherData = await response.json();
-  return weatherData;
+  try {
+    const response = await fetch(
+      'https://api.weatherapi.com/v1/forecast.json?key=29061576e8ad448eaef193306241202&q=Paris&days=3',
+      { mode: 'cors' },
+    );
+
+    if (!response.ok) {
+      throw new Error(`Network response was not ok: ${response.status}`);
+    }
+    const weatherData = await response.json();
+    return weatherData;
+  } catch (error) {
+    console.error('Error fetching weather data: ', error);
+    return null;
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import './styles.css';
 import getWeather from './getWeather';
 
-const weatherData = await getWeather().catch((err) => { console.log(err); });
-console.log(weatherData);
-console.log(`The weather in ${weatherData.location.name} is ${weatherData.forecast.forecastday[0].day.avgtemp_c} celcius`);
+async function showWeather() {
+  try {
+    const weatherData = await getWeather();
+    console.log(weatherData);
+    console.log(`The weather in ${weatherData.location.name} is ${weatherData.forecast.forecastday[0].day.avgtemp_c} celcius`);
+  } catch (error) {
+    console.error('Failed to get weather data: ', error);
+  }
+}
+
+showWeather();
